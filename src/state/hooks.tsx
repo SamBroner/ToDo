11e545acfc3ee0c38cbb8b5b31c7bb3da81cf506
Sam Broner <sabroner@microsoft.com>
2020-10-25 19:22:21 -0700

    Solved. You can't use the useContext fn in a callback. It needs to be accesed via scope

--- src/state/hooks.tsx
+++ src/state/hooks.tsx
@@ -30,50 +30,77 @@ export const useSelector = <T,>(selectorFunction: (dataObject: Main) => T, event
 }
 
 export const useDispatch = () => {
+    const dataObject = React.useContext(FluidContext);
+    const { myDir } = dataObject;
+
     return {
-        addItem,
-        deleteItem,
-        updateItem
+        addItem: () => {
+            // Would be params
+            const {id, item} = {
+                id: Date.now().toString(),
+                item: createListItems(1)[0],
+            }
+        
+            item.key = id;
+            const subdir = myDir.createSubDirectory(item.key);
+            for (const k in item) {
+              subdir.set(k, item[k]);
+            }
+        },
+        deleteItem: (key: string) => {
+            if (myDir.hasSubDirectory(key)) {
+              myDir.deleteSubDirectory(key);
+              dataObject.emitEvent("directoryChanged");
+            }
+            return;
+        },
+        updateItem: (id: string, updates: Partial<Omit<IExampleItem, "id">>) => {        
+            const subDir = myDir.getSubDirectory(id);
+            for (const key in updates) {
+              subDir?.set(key, updates[key])
+            }
+            return;
+        }
     }
 }
 
-/**
- * Doesn't take a parameter because we use Fluent's auto generator
- */
-export const addItem = () => {
-    const dataObject = React.useContext(FluidContext);
-    const { myDir } = dataObject;
+// /**
+//  * Doesn't take a parameter because we use Fluent's auto generator
+//  */
+// export const addItem = () => {
+//     const dataObject = React.useContext(FluidContext);
+//     const { myDir } = dataObject;
 
-    // Would be params
-    const {id, item} = {
-        id: Date.now().toString(),
-        item: createListItems(1)[0],
-    }
+//     // Would be params
+//     const {id, item} = {
+//         id: Date.now().toString(),
+//         item: createListItems(1)[0],
+//     }
 
-    item.key = id;
-    const subdir = myDir.createSubDirectory(item.key);
-    for (const k in item) {
-      subdir.set(k, item[k]);
-    }
-}
+//     item.key = id;
+//     const subdir = myDir.createSubDirectory(item.key);
+//     for (const k in item) {
+//       subdir.set(k, item[k]);
+//     }
+// }
 
-export const deleteItem = (key: string) => {
-    const dataObject = React.useContext(FluidContext);
-    const { myDir } = dataObject;
+// export const deleteItem = (key: string) => {
+//     const dataObject = React.useContext(FluidContext);
+//     const { myDir } = dataObject;
 
-    if (myDir.hasSubDirectory(key)) {
-      myDir.deleteSubDirectory(key);
-      dataObject.emitEvent("directoryChanged");
-    }
-    return;
-}
+//     if (myDir.hasSubDirectory(key)) {
+//       myDir.deleteSubDirectory(key);
+//       dataObject.emitEvent("directoryChanged");
+//     }
+//     return;
+// }
 
-export const updateItem = (id: string, updates: Partial<Omit<IExampleItem, "id">>) => {
-    const { myDir } = React.useContext(FluidContext);
+// export const updateItem = (id: string, updates: Partial<Omit<IExampleItem, "id">>) => {
+//     const { myDir } = React.useContext(FluidContext);
 
-    const subDir = myDir.getSubDirectory(id);
-    for (const key in updates) {
-      subDir?.set(key, updates[key])
-    }
-    return;
-}
+//     const subDir = myDir.getSubDirectory(id);
+//     for (const key in updates) {
+//       subDir?.set(key, updates[key])
+//     }
+//     return;
+// }
